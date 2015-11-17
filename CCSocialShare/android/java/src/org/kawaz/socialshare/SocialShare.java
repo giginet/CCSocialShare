d721bccdedd7a6a86d870795fb12fb18bfd936b0
giginet <giginet.net@gmail.com>
2015-11-17 19:52:44 +0900

    Add image attachment feature to Android

--- CCSocialShare/android/java/src/org/kawaz/socialshare/SocialShare.java
+++ CCSocialShare/android/java/src/org/kawaz/socialshare/SocialShare.java
@@ -1,33 +1,69 @@
 package org.kawaz.socialshare;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SocialShare {
     static public void postToTwitter(String message, String imagePath) {
         String url = "http://twitter.com/share?text=" + message;
         Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(url));
+        intent.putExtra(Intent.EXTRA_TEXT, message);
         if (!imagePath.equals("")) {
-            System.out.println(imagePath);
-            System.out.println("image path!!!!!!");
             try {
-                File file = new File(imagePath);
-                System.out.println("File exist");
-                intent.putExtra(Intent.EXTRA_TEXT, message);
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setType("image/png");
-                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                Uri originalImageUri = Uri.parse(imagePath);
+                String ext = getExtension(originalImageUri);
+                Uri readableFileUri = saveImageToExternalDirectory(originalImageUri);
+                if (readableFileUri != null) {
+                    intent.setType("image/" + ext);
+                    intent.putExtra(Intent.EXTRA_STREAM, readableFileUri);
+                }
             } catch (Exception e) {
             }
         }
         Context context = Cocos2dxActivity.getContext();
         context.startActivity(intent);
+    }
+
+    static private String getExtension(Uri imagePath) {
+        String pathString = imagePath.getPath();
+        return pathString.substring(pathString.lastIndexOf(".") + 1);
+    }
+
+    static private String getFilename(Uri imagePath) {
+        return imagePath.getLastPathSegment();
+    }
+
+    static private Uri saveImageToExternalDirectory(Uri imageUri) {
+        File dst = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), getFilename(imageUri));
+        File src = new File(imageUri.getPath());
+        try {
+            copyFile(src, dst);
+        } catch (Exception e) {
+            return null;
+        }
+        return Uri.fromFile(dst);
+    }
+
+    static public void copyFile(File src, File dst) throws Exception {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
     }
 }
